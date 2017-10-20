ee1619506cd238cba9602dde557a0fe58258b07b
Fernando Garcia <jfer.garciav@gmail.com>
2017-10-20 10:44:01 -0500

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,64 @@
+import os
+wrong_letters = set()
+assert_letters = set()
+wrongs_counter = 0
+
+_,_ = os.system('cls'), os.system('clear')
+print("*" * 50)
+print("{:^50}".format("JUEGO DEL AHORCADO"))
+print("*" * 50)
+
+word = input("\n\n\nIngresa la palabra secreta: ").lower()
+_,_ = os.system('cls'), os.system('clear')
+
+while True:
+    print("\n\nPalabra secreta:", end=" ")
+    for letter in word:
+        print(letter, end=" ") if letter in assert_letters else print("_", end=" ")
+
+    letter = input("\n\nIngresa una letra: ").lower()
+
+    if letter in word and letter not in assert_letters:
+        assert_letters.add(letter)
+        if assert_letters == set(word):
+            print("\n\t -- GANASTE --")
+            print("\t La palabra secreta era: {word}".format(word=word))
+            break
+    else:
+        wrongs_counter += 1
+        wrong_letters.add(letter)
+        print("\n\t ERROR.")
+
+        if wrongs_counter == 1:
+            print(""" 
+                     () 
+                """)
+        elif wrongs_counter == 2:
+            print(""" 
+                     ()
+                     || 
+                """)
+        elif wrongs_counter == 3:
+            print(""" 
+                     ()
+                     ||\ 
+                """)
+        elif wrongs_counter == 4:
+            print(""" 
+                     ()
+                    /||\ 
+                """)
+        elif wrongs_counter == 5:
+            print(""" 
+                     ()
+                    /||\ 
+                     /
+                """)
+        elif wrongs_counter == 6:
+            print(""" 
+                     ()
+                    /||\ 
+                     /\
+                """)
+            print("\n\t -- PERDISTE --")
+            break
